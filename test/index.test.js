311e1a8a4fa56d260641d78ceaf613db1da0312f
Kai Pahl <kai@kaipahl.de>
2018-06-25 14:44:53 +0200

    Add methods to recognize file data

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
-// var assert = require('assert');
-var
+'use strict';
+
+let
 	renameFm4 = require('../lib/index.js'),
 	chai = require('chai'),
 	expect = chai.expect
@@ -8,29 +9,36 @@ var
 
 
 
+// =============================================================================
+describe ('getFileId() - Get correct file id', function() {
+	let
+		fileNames = {
+			'1': 'index.html?channel=fm4&shoutcast=0&player=fm4_v1&referer=fm4.orf.at&_=1522729540062&userid=d96695eb-59cd-4970-a728-c70baf9d51a1&id=2018-03-31_1859_tl_54_7DaysSat17_55161.mp3&offset=175000'
+		};
+
+	it ('... should get the correct file ids', function() {
+		expect ( renameFm4.getFileId( fileNames['1'] ) ).to.equal('2018-03-31_1859_tl_54_7DaysSat17');
+	});
+
+});
+
+
 
 // =============================================================================
-describe ('Read files', function() {
+describe ('getMetaData() - Get correct file metadata from file id', function() {
+	let
+		fileIds = {
+			'1': '2018-03-31_1859_tl_54_7DaysSat17'
+		};
 
-	describe ('getHandlebarsLayout()', function() {
-		var
-			fileExists = tug.getHandlebarsLayout( 'index.hbs' ),
-			fileWithSlash = tug.getHandlebarsLayout( '/index.hbs' ),
-			fileNoLayout = tug.getHandlebarsLayout( '' ),
-			fileUndefined = tug.getHandlebarsLayout( ),
-			fileWithoutExtension = tug.getHandlebarsLayout( 'index' ),
-			fileDoesntExist = tug.getHandlebarsLayout( 'dajhgDFJHadsfjsdFJGsdjh.hbs' )
-		;
-
-		it ('... should get the correct layout files', function() {
-			expect ( fileExists ).to.match(/Here is index/);
-			expect ( fileWithSlash ).to.match(/Here is index/);
-			expect ( fileNoLayout ).to.match(/Here is default/);
-			expect ( fileUndefined ).to.match(/Here is default/);
-			expect ( fileWithoutExtension ).to.match(/Here is index/);
-			expect ( fileDoesntExist ).to.match(/Here is default/);
+	it ('... should get the correct meta data', function() {
+		expect ( renameFm4.getMetaData( fileIds['1'] ) ).to.eql({
+			yyyy: '2018',
+			mm: '03',
+			dd: '31',
+			dayOfWeek: 'Sat',
+			startTime: '1859'
 		});
-
 	});
 
 });
